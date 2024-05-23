a43d74808a56ea156f2e31b1db76fbcf583c4d55
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-05-23 22:45:39 +1000

    Add new defaults

--- typhoidsim/defaults.py
+++ typhoidsim/defaults.py
@@ -6,6 +6,12 @@ import numpy as np
 import sciris as sc
 
 
+# Datetime
 months_per_year = 12   # Months per year
 days_per_year   = 365    # Not quite, because of leap years ...
+
+# Numeric
 eps             = np.finfo(np.float64).resolution  # To avoid divisions by-zero
+
+# Plotting defaults
+default_plot_granularity = 512
