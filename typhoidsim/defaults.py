90f3f5f3f5a9d5bdd41121a20907f651a57106aa
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-05-31 09:55:52 +1000

    Add days_per_week to transform parameters expressed in weeks

--- typhoidsim/defaults.py
+++ typhoidsim/defaults.py
@@ -8,7 +8,8 @@ import sciris as sc
 
 # Datetime
 months_per_year = 12   # Months per year
-days_per_year   = 365    # Not quite, because of leap years ...
+days_per_year   = 365  # Not quite, because of leap years ...
+days_per_week   = 7    #
 
 # Numeric
 eps             = np.finfo(np.float64).resolution  # To avoid divisions by-zero
