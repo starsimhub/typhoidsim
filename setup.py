57496a4f05d1ea9280f9ffefd02b143098489db4
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-06-14 13:38:22 +1000

    Bump starsim version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'scipy',
         'pandas>=2.0.0',
         'sciris>=3.1.6',
-        'starsim>=0.5.0',
+        'starsim>=0.5.2',
         'matplotlib',
         'numba',
         'networkx',
