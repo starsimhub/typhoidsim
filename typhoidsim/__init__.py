281f0e3c4c12177d0b6c963db4adb7b4c0a2e5e3
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-06-15 15:31:51 +1000

    Update imports

--- typhoidsim/__init__.py
+++ typhoidsim/__init__.py
@@ -1,6 +1,8 @@
 from .version import __version__, __versiondate__
 from .utils import *
+from .utils_math import *
 from .defaults import *
 from .patterns import *
 from .typhoid import *
 from .treatments import *
+from .settings import *
