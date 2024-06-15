75f1f4ac48c98fede27c19999a0a363c1c63ee78
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-06-15 15:31:05 +1000

    Draft functionality to read small csv datasets

--- typhoidsim/utils.py
+++ typhoidsim/utils.py
@@ -1,37 +1,68 @@
 """
-Numerical utilities
+General utilities
 """
-
+import os
 import warnings
+
 import numpy as np
-import sciris as sc
-import starsim as ss
 import numba as nb
 import pandas as pd
 
+import sciris as sc
+import starsim as ss
 
+import typhoidsim as ty
 
 # Specify all externally visible things this file defines
-__all__ = ['sigmoid']
+__all__ = ['get_data_home', 'load_dataset']
 
 
-def sigmoid(x, max_x, slope):
+def get_data_home(data_home=None):
     """
-    Compute a sigmoid-like function. The range of x is between [0, max_x].
+    Return a path to the directory for default datasets.
+    This funciton is needed by `load_dataset()`, and avoids the
+    problem of using relative paths.
 
-    This function is used in the age-based exposure mechanism in typhoid.
+    If the ``data_home`` argument is not provided, it will use a directory
+    specified by the `TYPHOIDSIM_DATA` environment variable (if it exists)
+    or otherwise it will use the default data directory.
+    """
+
+    if data_home is None:
+        data_home = os.environ.get("TYPHOID_DATA", ty.options.data_dir)
+    data_home = os.path.expanduser(data_home)
+    if not os.path.exists(data_home):
+        os.makedirs(data_home)
+    return data_home
+
+
+def load_dataset(ds_name, data_home=None, **kwargs):
+    """
+    Load default dataset from typhoidsim data directory.
+
+    This function provides access to a small collection of datasets
+    that are useful to set empirical distributions (ie, demographics, or
+    gallstone probs by age and gender), rather than having those hardcoded
+    in the code.
+
+    The small datasets are expected to be simple tabular data in saved in csv
+    files. This function may apply some small amount of preprocessing, but it's
+    not intended to be a full ingest and preprocessing pipelines. The csv files
+    are expected to be in an already 'ingestable' form and simply loaded with
+    pandas.read_csv().
+
+    Use `get_dataset_names` to see a list of available datasets.
 
     Arguments:
-      x (array-like): The array of x values at which the function is evaluated
-      max_x (float): The asymptote of the function as x approaches infinity.
-      slope (float): The slope parameter that controls how 'fast' we reach
-        100% exposure (or 100% susceptible). If slope=0, this function is
-        a linear function, if slope > 0, higher susceptibility is achieved
-        faster. If slope < 0, higher susceptibility is reached slower.
+      ds_name (str): name of the dataset (``{name}.csv``).
+      data_home (str/Path, optional): the directory in which to cache data.
+      kwargs: additional keyword arguments passed through to `pandas.read_csv`.
 
     Returns:
-      y : (ndarray)
-        An array of y values corresponding to the sigmoid-like function
-        evaluated at the input x values -- which are expected to represent age.
+       df(`pandas.DataFrame`): tabular data, with some preprocessing applied
+       (depends on the dataset)
+
+    # Inspired by seaborn example datasets functionality
     """
-    return 1.0 - (max_x - x) / (x * slope + max_x)
+
+    pass
