a4c46e4f77851e69dd33526038dd470e1ef7476c
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-07-04 15:51:35 +1000

    Update required starsim version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'scipy',
         'pandas>=2.0.0',
         'sciris>=3.1.6',
-        'starsim>=0.5.2',
+        'starsim>=0.5.9',
         'matplotlib',
         'numba',
         'networkx',
