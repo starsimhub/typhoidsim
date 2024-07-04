228093ff41a5ce20224d47147054c275e0f52790
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-07-04 17:38:01 +1000

    [WIP] #39 Remove exposed state

--- typhoidsim/analyzers.py
+++ typhoidsim/analyzers.py
@@ -29,27 +29,31 @@ class states_consistency(ss.Analyzer):
         typ = sim.diseases.typhoidsimple
 
         # Mutually exclusive estates
-        mut_exc_1 = ~(typ.immune & typ.susceptible & typ.exposed & typ.prepatent & typ.acute & typ.subclinical & typ.chronic & typ.recovered).any()
+        mut_exc_1 = ~(typ.immune & typ.susceptible & typ.prepatent & typ.acute & typ.subclinical & typ.chronic & typ.recovered).any()
         mut_exc_2 = ~(typ.asymptomatic & typ.symptomatic).any()
         mut_exc_3 = ~(typ.susceptible & typ.infected).any()
+        mut_exc_4 = ~(typ.immune & typ.infected).any()
 
         if not mut_exc_1:
             raise ValueError('Individual Boolean States should be mutually exclusive but are not.')
 
         if not mut_exc_2:
-            raise ValueError('States Susceptible and Infected should be mutually exclusive but are not.')
-
-        if not mut_exc_3:
             raise ValueError('States Symptomatic and Asymptomatic should be mutually exclusive but are not.')
 
+        if not mut_exc_3:
+            raise ValueError('States Susceptible and Infected should be mutually exclusive but are not.')
+
+        if not mut_exc_4:
+            raise ValueError('States Immune and Infected should be mutually exclusive but are not.')
+
         # Collectively ehaustive
-        coll_exh = (typ.immune | typ.susceptible | typ.exposed | typ.prepatent | typ.acute | typ.subclinical | typ.chronic | typ.recovered | sim.people.dead).all()
+        coll_exh = (typ.immune | typ.susceptible | typ.prepatent | typ.acute | typ.subclinical | typ.chronic | typ.recovered | sim.people.dead).all()
 
         if not coll_exh:
             breakpoint()
-            raise ValueError('Invidividual Boolean States should be collectively exhaustive but are not.')
+            raise ValueError('Individual Boolean States should be collectively exhaustive but are not.')
 
-        checkall = np.array([mut_exc_1, mut_exc_2, mut_exc_3, coll_exh])
+        checkall = np.array([mut_exc_1, mut_exc_2, mut_exc_3, mut_exc_4, coll_exh])
         if not checkall.all():
             self.success = False
         return
