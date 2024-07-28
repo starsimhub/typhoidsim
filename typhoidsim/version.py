e486d6c0c8283ef7589975bb1c86fc11f1f80d20
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-07-28 21:00:51 +1000

    Update version and date

--- typhoidsim/version.py
+++ typhoidsim/version.py
@@ -4,7 +4,7 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.1.0-dev'
-__versiondate__ = '2024-07-04'
+__version__ = '0.2.0-dev'
+__versiondate__ = '2024-07-28'
 __license__ = (f'Typhoidsim {__version__} ({__versiondate__}) — © 2024 by '
                f'IDM')
