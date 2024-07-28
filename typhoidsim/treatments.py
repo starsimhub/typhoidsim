af864f93be9d4cebb63aa4ea5164bd0266cafd74
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-07-28 14:20:28 +1000

    Implements typhoid infection clearence, with clearence rate parameter

--- typhoidsim/treatments.py
+++ typhoidsim/treatments.py
@@ -28,7 +28,15 @@ class acute_treatment(ss.Intervention):
     Treat acute (symptomatic) subjects.
 
     This is expected to use the "infectiousness_redux" product, which
-    results in a reduction or blocking in shedding
+    results in an effective reduction or blocking in shedding, by reducing
+    an agent's infectiousness level.
+
+    For instance, Infectiousness is constant throughout the acute duration and is
+    determined TAI (Typhoid Acute Infectiousness). This is mediated by the
+    product's treatment multiplier when an individual seeks treatment.
+    For an agent who sought and received treatment,
+    from treatment day to the end of the stage, its new infectiousness level is
+    I(Acute) = TAI * product treatment multiplier
 
     The basic mechanics of this treatment are:
     - 0. Find candidate agents:
@@ -38,9 +46,8 @@ class acute_treatment(ss.Intervention):
             be under treatment until they are no longer acute
     - 1. Of the ones that are eligible to 'seek' treatment today, decide who
          does and who doesnt't
-    - 2. Treat new and previous candidates
-    - 3. Count how many people receive treatment today (includes new
-         and previous candidates)
+    - 2. Treat new candidates
+    - 3. Count how many people receive treatment today
     """
 
     def __init__(self, product=None, prob=1.0, eligibility=None, **kwargs):
@@ -55,53 +62,131 @@ class acute_treatment(ss.Intervention):
 
     def init_pre(self, sim):
         super().init_pre(sim)
-        self.results += ss.Result(self.name, 'new_treated', sim.npts, dtype=int)  # only counts those who started today
         self.results += ss.Result(self.name, 'n_treated', sim.npts, dtype=int)  # count how many were treated today, includes new and old patients
         self.initialized = True
         return
 
     def apply(self, sim):
-        new_candidates, under_treatment = self.get_eligible(sim)
+        new_candidates = self.get_eligible(sim)
         receive_uids = self.coverage_dist.filter(new_candidates)
-        if len(receive_uids):
+        newly_treated = len(receive_uids)
+        if newly_treated:
             self.product.administer(sim.people, receive_uids)
             self.treated[receive_uids] = True
             # How may accepted and started treatment today
-            newly_treated = self.treated[receive_uids].sum()
-        else:
-            newly_treated = 0.0
-
-        if len(under_treatment):
-            self.product.administer(sim.people, under_treatment)
-            recurrent = self.treated[under_treatment].sum()
-        else:
-            recurrent = 0.0
-
-        self.results['new_treated'][sim.ti] = newly_treated
-        self.results['n_treated'][sim.ti] = newly_treated + recurrent
-
+        self.results['n_treated'][sim.ti] = newly_treated
         return
-
-    def check_eligibility(self, sim, uids):
-        may_seek_treatment_today = (sim.typhoidsimple.ti_seek_treatment == sim.ti).uids
-        return may_seek_treatment_today
 
     def get_eligible(self, sim):
         """
         Get candidates for treatment on this timestep. This includes new patients
         and old patients.
         """
+        # TODO: use self.eligibility
         # Only agents experience the acute stage of infection
         acute_uids = (sim.people.typhoidsimple.acute).uids
         # Those who would seek treatment today
         seeks_treatment = (sim.people.typhoidsimple.ti_seek_trtmnt == sim.ti).uids
         new_candidates = acute_uids.intersect(seeks_treatment)
+        return new_candidates
+
+
+class infection_clearence(ss.Intervention):
+    """
+    Clears an infected individual's Typhoid infection. It will clear Typhoid
+    infections of all types (prepatent, acute, subclinical, chronic)
+
+    This is expected to use the "infectiousness_clearence" product, which
+    results in an effective reduction or blocking in shedding, by reducing
+    an agent's infectiousness level at every timestep.
+
+    The basic mechanics of this treatment are:
+    - 0. Find candidate agents:
+       - a. new candidates: infected agents
+       - b. previous candidates: people under treatment continue to
+            be under treatment until they are no longer infected.
+    - 2. Treat new candidates AND patients under treatment
+    - 3. Count how many people receive treatment today and how many NEW started treatment today
+    """
+
+    def __init__(self, product=None, eligibility=None, **kwargs):
+        super().__init__(**kwargs)
+        self.eligibility = eligibility
+        self._parse_product(product)
+        self.treated = ss.BoolArr('treated')
+        self.results = ss.ndict()
+        return
+
+    def init_pre(self, sim):
+        super().init_pre(sim)
+        self.results += ss.Result(self.name, 'n_treated', sim.npts, dtype=int)  # count how many were treated today, includes new and old patients
+        self.results += ss.Result(self.name, 'n_started_tr', sim.npts, dtype=int)  # count how many started treatment today, includes only new patients
+        self.initialized = True
+        return
+
+    def apply(self, sim):
+        new_patients, under_treatment = self.get_eligible(sim)
+        newly_treated = len(new_patients)
+        all_treated = newly_treated + len(under_treatment)
+
+        if len(new_patients):
+            self.product.administer(sim.people, new_patients)
+            self.treated[new_patients] = True
+
+        if len(under_treatment):
+            self.product.administer(sim.people, under_treatment)
+
+        self.results['n_started_tr'][sim.ti] = newly_treated
+        self.results['n_treated'][sim.ti] = all_treated
+
+        # Check if infectiousness was cleared in this timestep
+        treated = ss.uids.cat(new_patients, under_treatment)
+        cleared_uids = (sim.people.typhoidsimple.infectiousness[treated] <= 0.0).uids
+        if len(cleared_uids):
+            # Reset infectiousness
+            sim.people.typhoidsimple.infectiousness[cleared_uids] = 0.0
+
+            # Reset infected states
+            for state in [
+                "prepatent",
+                "acute",
+                "subclinical",
+                "chronic",
+            ]:
+               sim.people.typhoidsimple.statesdict[state][cleared_uids] = False
+
+            # Reset time of death if this patient was supposed die
+            for state in [
+                "ti_prepatent",
+                "ti_acute",
+                "ti_seek_trtment",
+                "ti_subclinical",
+                "ti_chronic",
+                "ti_dead",
+            ]:
+                sim.people.typhoidsimple.statesdict[state][cleared_uids] = np.nan
+
+            # Set recovered state and when this agent becomes susceptible
+            sim.people.typhoidsimple.statesdict["recovered"][cleared_uids] = True
+            sim.people.typhoidsimple.statesdict["ti_susceptible"][cleared_uids] = sim.ti + 1
+
+        return
+
+    def get_eligible(self, sim):
+        """
+        Get candidates for treatment on this timestep. This includes new patients
+        and old patients.
+        """
+        # TODO: use self.eligibility
+        # Only agents experience the acute stage of infection
+        infected_uids = (sim.people.typhoidsimple.infected).uids
 
         # Those who are under treatment
-        under_treatment = (sim.people.acute_treatment.treated).uids
+        under_treatment = (sim.people.infection_clearence.treated).uids
         # and still are in the acute stage
-        patients = acute_uids.intersect(under_treatment)
-        return new_candidates, patients
+        old_patients = infected_uids.intersect(under_treatment)
+        new_patients = infected_uids.intersect(~under_treatment)
+        return new_patients, old_patients
 
 
 class environmental_intervention(ss.Intervention):
@@ -112,16 +197,7 @@ class environmental_intervention(ss.Intervention):
     pass
 
 
-
 # - Products
-class infection_clearence(ss.Intervention):
-    """
-    Individual-targeted intervention immediately clears an infected individual’s
-    Typhoid infection (and expires).
-    """
-    pass
-
-
 class infectiousness_redux(ss.Product):
     """
     Reduction in infectiousness. This product is applied to acute cases only
@@ -136,4 +212,28 @@ class infectiousness_redux(ss.Product):
 
     def administer(self, people, uids):
         people.typhoidsimple.infectiousness[uids] *= self.pars.multiplier
+        return
+
+
+class infectiousness_clearence(ss.Product):
+    """
+    Reduction in infectiousness. This product is applied to acute cases only
+    and results in a reduction or blocking in shedding.
+    """
+
+    def __init__(self, pars=None, *args, **kwargs):
+        super().__init__()
+        self.default_pars(clearence_rate=0.2, dt=1.0)  # in fraction of CFUs per day that are cleared
+        self.update_pars(pars, **kwargs)
+        return
+
+    def init_pre(self, sim):
+        super().init_pre(sim)
+        self.pars.dt = sim.dt
+        self.initialized = True
+        return
+
+    def administer(self, people, uids):
+        clearence = people.typhoidsimple.infectiousness[uids] * self.pars.clearence_rate * self.pars.dt   # multiply by dt for cases when dt < 1
+        people.typhoidsimple.infectiousness[uids] -= clearence
         return
