01a11ae842b279db0dc742c761a7d1695d3c7717
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-07-29 23:33:11 +1000

    Test typhoid defaults without any interventions

--- tests/dev_tests/test_baselines.py
+++ tests/dev_tests/test_baselines.py
@@ -20,24 +20,7 @@ pars = sc.objdict(
     rand_seed=2,  # Set a non-default seed
 )
 
-
-# my_product = ty.infectiousness_redux(multiplier=0.5)
-#
-# my_intervention = ty.acute_treatment(
-#     prob=1.0,             # probability of seeking treatment when acute
-#     product=my_product,  # use basic treatment that reduces infectiousness
-# )
-
-#my_intervention = ty.base_test()
-
-efficacy_pattern = ty.Pattern("average_efficacy + amp * cos((2*pi/period)*var)",
-                              pars={'average_efficacy': 0.9, 'amp': 0.1, 'period': ty.days_per_year/4, 'pi': 3.141592653589793})
-
-my_intervention = ty.environmental_intervention(pattern=efficacy_pattern,
-                                                target_factor="env2ppl_exposure_rate")
-
 typhoid = ty.Typhoid()
-
 
 ppl = ss.People(10000)
 
@@ -48,7 +31,6 @@ sim = ss.Sim(
     pars=pars,
     networks=network,
     diseases=typhoid,
-    interventions=my_intervention,
     )
 
 sim.run()
