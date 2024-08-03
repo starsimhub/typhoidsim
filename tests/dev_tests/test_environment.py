eaf21a0830ec196b710b02e4b7784834ad6151bb
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-08-03 15:33:25 +1000

    Write basic script to test environment module

---
+++ tests/dev_tests/test_environment.py
@@ -0,0 +1,41 @@
+"""
+
+"""
+import numpy as np
+import networkx as nx
+
+import matplotlib.pyplot as plt
+
+import sciris as sc
+import starsim as ss
+import typhoidsim as ty
+
+# Define the parameters
+n_years = 2  # Number of actual years to simulate
+pars = sc.objdict(
+    start=2000,  # Starting day/year
+    n_years=n_years*ty.days_per_year,  # Number of days to simulate
+    dt=1.0,       # Timestep expressed in days
+    verbose=0,    # Don't print details of the run
+    rand_seed=2,  # Set a non-default seed
+)
+
+# Who
+ppl = ss.People(10000)
+
+# What
+typhoid = ty.Typhoid()
+
+# How
+environment = ty.EnvironmentalPool()
+
+#
+sim = ss.Sim(
+    pars=pars,
+    diseases=typhoid,
+    demographics=environment,
+    )
+
+sim.run()
+sim.plot()
+plt.show()
