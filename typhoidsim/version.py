1077dcd6d051f47561d574e152ad4e0d481b7376
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-09-06 17:46:43 +1000

    Bump version (synthpop dependecy)

--- typhoidsim/version.py
+++ typhoidsim/version.py
@@ -4,7 +4,7 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.2.1-dev'
+__version__ = '0.2.2-dev'
 __versiondate__ = '2024-09-06'
 __license__ = (f'Typhoidsim {__version__} ({__versiondate__}) — © 2024 by '
                f'IDM')
