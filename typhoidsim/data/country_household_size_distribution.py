11edda6b1b3e6897435d18471372d01971bb6d27
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-09-12 14:29:44 +1000

    Add dist of household sizes for Chile and Pakistan

---
+++ typhoidsim/data/country_household_size_distribution.py
@@ -0,0 +1,16 @@
+"""
+Household size distribution from:
+https://www.un.org/development/desa/pd/sites/www.un.org.development.desa.pd/files/undesa_pd_2022_hh-size-composition.xlsx
+"""
+
+
+data = {
+     'Chile':     {'1':   20.17,   # Chile: Data Soruce:IPUMS: Ref. year: 2017
+                   '2-3': 44.75,
+                   '4-5': 27.47,
+                   '6+':   7.6},
+     'Pakistan':  {'1':    1.08,   # Pakistan: Data Source: DHS; Ref. year: 2013
+                   '2-3': 12.88,
+                   '4-5': 25.16,
+                   '6+':  60.88},
+}
