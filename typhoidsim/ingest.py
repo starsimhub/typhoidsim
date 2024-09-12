5e659fb672e036908387b68c087fb9bf3ad69c0b
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-09-12 13:07:08 +1000

    Wrap household size from synthpops

--- typhoidsim/ingest.py
+++ typhoidsim/ingest.py
@@ -4,7 +4,7 @@ loading data from files, parsing data and reformatting data, and processing
 so they tcan be consumed by typhoidsim or starsim.
 """
 
-__all__ = ['get_age_distribution']
+__all__ = ['get_age_distribution', 'get_household_distribution']
 
 
 def get_age_distribution(location):
@@ -24,3 +24,19 @@ def get_age_distribution(location):
     # with the lower bound of each age bin and the count/proportion in that age bin
     age_data = spl.get_age_distribution(location)[:, [0, 2]]
     return age_data
+
+
+def get_household_size(location):
+    """
+    A small wrapper of synthpops.people.loaders.get_household_size().
+    This function returns the average household size for that location. This
+    parameter is necessary when creating a HouseholdNet.
+
+    Args:
+        location (str): the name of the country
+
+    Returns:
+        household_size (float): the average household size of 'location'.
+    """
+    import synthpops.people.loaders as spl
+    return spl.get_household_size(location)
