11de15db3bf033794c3a0b83214217d490928086
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-09-13 21:11:30 +1000

    [WIP]: #71 draft household network and some ancilliary functions to handle household data

---
+++ typhoidsim/networks.py
@@ -0,0 +1,168 @@
+import numpy as np
+import numba as nb
+import sciris as sc
+import starsim as ss
+
+
+ss_float_ = ss.dtypes.float
+ss_int_ = ss.dtypes.int
+
+from .ingest import get_household_size_distribution, get_household_head_age_by_size_distribution
+
+
+class HouseholdNet(ss.DynamicNetwork):
+    """
+    Microstructured connectivity between agents. Households can change in size
+    if one of their members die.
+    """
+
+    def __init__(self, pars=None, key_dict=None, **kwargs):
+        """ Initialize """
+        super().__init__(key_dict=key_dict)
+        self.default_pars(
+            n_contacts=ss.constant(10),
+            location=None,
+            dur=20, # average duration of a household in years
+        )
+        self.update_pars(pars, **kwargs)
+        self.dist = ss.Dist(distname='HouseholdNet')  # Default RNG
+        return
+
+    def init_post(self):
+        self.add_pairs()
+        return
+
+    @staticmethod
+    @nb.njit(cache=True)
+    def get_source(inds, n_contacts):
+        """ Optimized helper function for getting contacts """
+        total_number_of_half_edges = np.sum(n_contacts)
+        count = 0
+        source = np.zeros((total_number_of_half_edges,), dtype=ss_int_)
+        for i, person_id in enumerate(inds):
+            n = n_contacts[i]
+            source[count: count + n] = person_id
+            count += n
+        return source
+
+    def get_contacts(self, inds, n_contacts):
+        """
+        Efficiently generate contacts
+
+        Note that because of the shuffling operation, each person is assigned 2N contacts
+        (i.e. if a person has 5 contacts, they appear 5 times in the 'source' array and 5
+        times in the 'target' array). Therefore, the `number_of_contacts` argument to this
+        function should be HALF of the total contacts a person is expected to have, if both
+        the source and target array outputs are used (e.g. for social contacts)
+
+        adjusted_number_of_contacts = np.round(number_of_contacts / 2).astype(cvd.default_int)
+
+        Whereas for asymmetric contacts (e.g. staff-public interactions) it might not be necessary
+
+        Args:
+            inds: List/array of person indices
+            number_of_contacts: List/array the same length as `inds` with the number of unidirectional
+            contacts to assign to each person. Therefore, a person will have on average TWICE this number
+            of random contacts.
+
+        Returns: Two arrays, for source and target
+        """
+        source = self.get_source(inds, n_contacts)
+        target = self.dist.rng.permutation(source)
+        self.dist.jump()  # Reset the RNG manually # TODO, think if there's a better way
+        return source, target
+
+    def update(self):
+        self.end_pairs()
+        self.add_pairs()
+        return
+
+    def add_pairs(self):
+        """ Generate contacts """
+        people = self.sim.people
+        born = people.alive & (people.age > 0)
+        if isinstance(self.pars.n_contacts, ss.Dist):
+            number_of_contacts = self.pars.n_contacts.rvs(
+                born.uids)  # or people.uid?
+        else:
+            number_of_contacts = np.full(len(people), self.pars.n_contacts)
+
+        number_of_contacts = sc.randround(number_of_contacts / 2).astype(ss_int_)  # One-way contacts
+
+        p1, p2 = self.get_contacts(born.uids, number_of_contacts)
+        beta = np.ones(len(p1), dtype=ss_float_)
+
+        if isinstance(self.pars.dur, ss.Dist):
+            dur = self.pars.dur.rvs(p1)
+        else:
+            dur = np.full(len(p1), self.pars.dur)
+
+        self.append(p1=p1, p2=p2, beta=beta, dur=dur)
+        return
+
+    def get_households(self):
+        """
+        Given a population of size n_agents, and a household size distribution
+        calculate the number of households of each size.
+
+        Returns:
+            An array with the count of households of size 's' at .
+        """
+        requested_n_agents = self.sim.pars.n_agents
+        if self.pars.location is not None:
+            hhs_dist = get_household_size_distribution(self.pars.location)
+            hhs_bins =  hhs_dist[:, 0]
+            hhs_props = hhs_dist[:, 1] / 100.0
+
+        else:
+            # Default household size distribution
+            hhs_bins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+            hhs_props = [0.25, 0.125, 0.125, 0.1, 0.1, 0.06, 0.06, 0.06, 0.06, 0.06]
+
+        household_size_dist = ss.choice(p=hhs_props, a=hhs_bins,
+                                        name='Household size distribution',
+                                        strict=False)
+
+        av_household_size = np.sum(hhs_bins*hhs_bins)
+        n_households = np.round(requested_n_agents / av_household_size)
+        n_households_by_size = np.zeros(len(hhs_bins), dtype=ss_int_)
+
+        for idx, prop in enumerate(hhs_props):
+            n_households_by_size[idx] = int(prop * n_households)
+
+        # Calculate number of agents we would have based on the household distributions
+        resulting_n_agents = np.sum([n_households_by_size[s - 1] * s for s in hhs_bins], dtype=int)
+
+        # Check difference between requested population size and expected based on household data
+        n_agents_diff = resulting_n_agents - requested_n_agents
+
+        while n_agents_diff != 0:
+            new_household_size = household_size_dist.rvs(1)
+            n_agents_diff, n_households_by_size = adjust_households(n_agents_diff,
+                                                                    n_households_by_size,
+                                                                    new_household_size)
+
+        return n_households_by_size
+
+
+def adjust_households(delta_agents, nhh_by_hs, delta_hh_size):
+    """
+    Adjusts the sizes of households based on a specified number of people that need
+    to be added or removed. The function selects a random household size that doesn't
+    exceed the remaining number of people that need to be adjusted, reduces the remaining
+    number by the chosen size
+
+    Args:
+        delta_agents (int): Number of agents to be added or removed. If
+        negative, people are added. If is positive, people are removed.
+        nhh_by_hs (np.array): number of household of a given size (current distribution/count) .
+        delta_hh_size (int): The size of the household to be added or removed.
+
+    Returns:
+       delta_agents (int): number of remaining agents to be added or removed
+       nhh_by_hs (np.array): adjusted number of household of a given size.
+    """
+    sign = -np.sign(delta_agents)
+    delta_agents = delta_agents + sign*delta_hh_size
+    nhh_by_hs[delta_hh_size - 1] = nhh_by_hs[delta_hh_size - 1] + sign
+    return delta_agents, nhh_by_hs
