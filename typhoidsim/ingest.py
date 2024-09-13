46a207af5e92740ef53ee15e7b6a70341fea6622
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-09-13 21:10:26 +1000

    [WIP]: #71 adding infrstructure to generate household networks with location-specific data

--- typhoidsim/ingest.py
+++ typhoidsim/ingest.py
@@ -8,9 +8,11 @@ import numpy as np
 import sciris as sc
 
 from .data import country_household_size_distribution as household_size_distribution
+from .data import country_household_head_age_distribution as household_head_age_distribution
 
 __all__ = ["get_age_distribution", "get_household_size",
-           "get_household_size_distribution"]
+           "get_household_size_distribution", "get_household_head_age_distribution",
+           "get_household_head_age_by_size_distribution"]
 
 
 def get_age_distribution(location):
@@ -56,7 +58,8 @@ def get_household_size_distribution(location=None):
         location (str or list): name of the country or countries to load the age distribution for
 
     Returns:
-        house_size (float): Size of household, or dict if multiple locations
+        household_size_distribution (np.array): Nx2 array. First column household size;
+        second column: fraction of household of that size
 
     """
     import synthpops.people.loaders as spl
@@ -84,7 +87,7 @@ def get_household_size_distribution(location=None):
         hhs_data = np.array(hhs_data)
         bins = np.hstack([np.arange(start, end + 1) for start, end in hhs_data[:, :2]])
         bin_widths = (hhs_data[:, 1] - hhs_data[:, 0] + 1).astype(int)
-        vals = np.repeat(hhs_data[:, 2] / bin_widths, bin_widths)
+        vals = np.repeat(hhs_data[:, 2] / bin_widths, bin_widths) / 100.0  # transform to proportion
         hh_size_dist = np.column_stack((bins, vals))
         result[loc] = hh_size_dist
 
@@ -92,3 +95,72 @@ def get_household_size_distribution(location=None):
         result = list(result.values())[0]
 
     return result
+
+
+def get_household_head_age_distribution(location=None):
+    """
+    Load household head's age distribution.
+
+    Args:
+        location (str or list): name of the country or countries to load the age distribution for
+
+    Returns:
+        household_head_age_distribution (float): N x 2: household head age bracket and proportion
+
+    """
+    import synthpops.people.loaders as spl
+
+    # Load the raw data
+    data = sc.dcp(household_head_age_distribution.data)
+
+    entries = spl.map_entries(data, location)
+
+    max_age = 100
+    result = {}
+
+    for loc, hh_ha_distribution in entries.items():
+        hhha_data = []  # (h)ouse(h)old (h)ead (a)ge
+        for hh_ha, hh_ha_perc in hh_ha_distribution.items():
+            if hh_ha[-1] == '+':
+                 val = [int(hh_ha[:-1]), max_age, hh_ha_perc]
+            else:
+                size = hh_ha.split('-')
+                val = [int(size[0]), int(size[1]), hh_ha_perc]
+            hhha_data.append(val)
+        hhha_data = np.array(hhha_data)
+        bins = np.hstack([np.arange(start, end + 1) for start, end in hhha_data[:, :2]])
+        bin_widths = (hhha_data[:, 1] - hhha_data[:, 0] + 1).astype(int)
+        vals = np.repeat(hhha_data[:, 2] / bin_widths, bin_widths) / 100.0  # transform to proportion
+        hh_ha_dist = np.column_stack((bins, vals))
+        result[loc] = hh_ha_dist
+
+    if len(result) == 1:
+        result = list(result.values())[0]
+
+    return result
+
+
+def get_household_head_age_by_size_distribution(location):
+    """
+    Calculate the contigency table, representing thr probabilities of each
+    combination of household size and head age. The assumption is
+    that household size and household head age are independent.
+
+    If they are not independent, then we would need the joint distribution
+    of these two variables which needs to be collected or estimated separately.
+
+    Args:
+        location (str or list): name of the country or countries to load the age distribution for
+
+    Returns:
+        household_head_age_distribution (float): N x 2: household head age bracket and proportion
+
+    """
+    import pandas as pd
+    hh_size_dist     = get_household_size_distribution(location)
+    hh_head_age_dist = get_household_head_age_distribution(location)
+
+    df = pd.DataFrame(np.outer(hh_size_dist[:, 1], hh_head_age_dist[:, 1]),
+                      columns=hh_head_age_dist[:, 0], index=hh_size_dist[:, 0])
+
+    return df
