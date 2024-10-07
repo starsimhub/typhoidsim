e214e9a5c42fbdc1957d883a76ab116a35fa3881
Cliff Kerr <questionably@gmail.com>
2024-10-07 11:56:23 -0700

    pin starsim version for now

--- typhoidsim/version.py
+++ typhoidsim/version.py
@@ -6,5 +6,4 @@ __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__ = '0.3.0-dev'
 __versiondate__ = '2024-10-05'
-__license__ = (f'Typhoidsim {__version__} ({__versiondate__}) — © 2024 by '
-               f'IDM')
+__license__ = f'Typhoidsim {__version__} ({__versiondate__}) — © 2024 by IDM'
