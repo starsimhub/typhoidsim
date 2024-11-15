fd22ea829256d42f08791ca2db808607c9a60db7
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-11-15 13:17:49 +1000

    Add functionality to parse age data from json file used in EMOD simulations; add function to quickly check if population age distribution looks ok

--- examples/calibration/calibration_pakistan_utils.py
+++ examples/calibration/calibration_pakistan_utils.py
@@ -6,9 +6,12 @@ The idea is that functions that are used multiple times are contained in one
 place, rather than being duplicated across multiple scripts.
 """
 
+import numpy as np
 import pandas as pd
 from functools import partial
 
+import sciris as sc
+import starsim as ss
 import typhoidsim as ty
 
 
@@ -39,9 +42,60 @@ def partial_env_trapezoidal(kwarg_pars):
     curent_modulation = my_modulation(t)
 
     Args:
-        kwarg_pars:
+        kwarg_pars of function typhoidsim.utils_math.asym_trapezoidal
 
     Returns:
-         callable: A partially applied function of ty.asym_trapezoidal
+         callable: A partially evaluated ty.asym_trapezoidal
     """
     return partial(ty.asym_trapezoidal, **kwarg_pars)
+
+
+def get_age_distribution_pakistan():
+    """
+    Parse data from json file used in EMOD simulations to get
+    age distribution for Pakistan. Returns it in a format that
+    can be passed to starsim's People, to build the appropriate population.
+    """
+    data_home = ty.get_data_home()
+    json_file = 'TestDemographics_pak_updated.json'
+    json_data = sc.loadjson(data_home + "/" + json_file)
+
+    # Lower edge of an age bin
+    age_bin_lb = np.array(json_data["Nodes"][0]["IndividualAttributes"]["AgeDistribution"]["ResultValues"])  # Drop the last value of age because is the right edge of the last age bin
+    age_cum_prob = json_data["Nodes"][0]["IndividualAttributes"]["AgeDistribution"]["DistributionValues"]
+    age_probs = np.diff(age_cum_prob)
+    df = pd.DataFrame({'age': age_bin_lb[0:-1], 'value': age_probs})
+    return df
+
+
+def check_age_distribution(n_agents=100_000):
+    """
+    Run a quick sim to get the age distribution of the population, and plots the
+    results.
+
+    Args:
+        n_agents (float): an integer number of agents that determines the size of the population.
+        Can be changed to assess finite size effects for small agent populations.
+
+    Returns:
+         None
+    """
+    import matplotlib.pyplot as plt
+
+    # Define the parameters
+    pars = sc.objdict(
+        start=2000,      # Starting year
+        n_years=0.125,   # Number of years to simulate
+        dt=1.0/365.0,    # Timestep of 1 day, expressed in years
+        verbose=0,       # Print details of the run
+        rand_seed=42,    # Set a non-default seed
+    )
+
+    ppl_pakistan = ss.People(n_agents,
+                             age_data=get_age_distribution_pakistan())
+    sim_pakistan = ss.Sim(pars=pars, people=ppl_pakistan)
+    sim_pakistan.run()
+
+    ty.plot_age_histogram(sim_pakistan.people)
+    plt.show()
+    return
