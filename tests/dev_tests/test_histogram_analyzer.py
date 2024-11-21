4cd8e15ced8c49918340c6f8072a68e01110d8d9
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-11-21 21:45:38 +1000

    Test histogram with nondefault values

--- tests/dev_tests/test_histogram_analyzer.py
+++ tests/dev_tests/test_histogram_analyzer.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 # Define high-level simulation parameters
 pars = dict(
-    start=2000,  # Starting year
-    n_years=1.0,  # Duration of the simulation in years
+    start=2000,    # Starting year
+    n_years=1.0,   # Duration of the simulation in years
     dt=1.0/365.0,  # Timestep of 1 day, expressed in years
-    verbose=1,  # Do not print details of the run
+    verbose=1,     # Do not print details of the run
 )
 
 ppl = ss.People(10_000)
@@ -18,13 +18,22 @@ tst = ty.base_test(prob_t=0.3, prob_tp=1.0, eligibility=ty.eligibility_by_age)
 age_bin_edges = [0, 2, 5, 10, 15, ty.max_age]
 age_bin_labels = ['<2', '2-4', '5-9', '10-14', '15+']
 # Track cases by age and by sex -- this analyzer returns counts in number of agents, not people. Scaling can be performed offline.
+
+to_record = dict(ti_infected=dict(path=("diseases", "typhoid")),
+                 alive=dict(path=("people",)),
+                 ti_positive=dict(path=("interventions", "base_test")),
+                 ti_tested=dict(path=("interventions", "base_test")),
+                 )
+
 anz_1 = ty.histograms_by_age_sex(age_bins=age_bin_edges,
                                  age_bin_labels=age_bin_labels,
                                  modality="counts",
+                                 to_record=to_record,
                                  name="hist_by_counts")
 
 anz_2 = ty.histograms_by_age_sex(age_bins=age_bin_edges,
                                  age_bin_labels=age_bin_labels,
+                                 to_record=to_record,
                                  modality="proportions",
                                  name="hist_by_props")
 
