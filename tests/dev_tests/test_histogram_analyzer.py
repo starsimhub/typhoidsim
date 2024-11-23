44d615ecaa504e48b7d2ddd1827b15125720cd5c
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-11-23 23:42:37 +1000

    Set n_agents to 10_000

--- tests/dev_tests/test_histogram_analyzer.py
+++ tests/dev_tests/test_histogram_analyzer.py
@@ -11,12 +11,12 @@ pars = dict(
 )
 
 ppl = ss.People(10_000)
-init_p = 0.5
+init_p = 0.5 # Large prevalence to see results
 typhoid = ty.Typhoid(pars={'init_prev': ss.bernoulli(p=init_p)})
 # create and apply the test intervention
 tst = ty.base_test(prob_t=0.3, prob_tp=1.0, eligibility=ty.eligibility_by_age)
-age_bin_edges = [0, 2, 5, 10, 15, ty.max_age]
-age_bin_labels = ['<2', '2-4', '5-9', '10-14', '15+']
+age_bin_edges = [0, 2, 5, 10, 15, 20, 40, 60, ty.max_age]
+age_bin_labels = ['<2', '2-4', '5-9', '10-14', '15-19', '20-39', '40-59', '60+']
 
 # Track cases by age and by sex -- this analyzer returns counts in number of agents, not people. Scaling can be performed offline.
 
@@ -29,8 +29,6 @@ age_bin_labels = ['<2', '2-4', '5-9', '10-14', '15+']
 anz_1 = ty.histograms_by_age_sex_monitor(age_bins=age_bin_edges,
                                          age_bin_labels=age_bin_labels,
                                          aggregate_sex=True,
-                                         aggregate_time="mean",
-                                         resampling_period=30.44/365.0,  # Duration of an average solar month
                                          name="hist_by_counts")
 
 
