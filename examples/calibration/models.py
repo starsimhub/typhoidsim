643e3572fa2a589bd5b48f405b041f00de99e0f2
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-11-23 14:11:26 +1000

    Compose simulations/scenarios

--- examples/calibration/models.py
+++ examples/calibration/models.py
@@ -1,16 +1,30 @@
-import matplotlib.pyplot as plt
+from functools import partial
 import numpy as np
 import pandas as pd
 import datetime
+import matplotlib.pyplot as plt
+
 
 import sciris as sc
 import starsim as ss
 import typhoidsim as ty
 
-import calibration_pakistan_utils as utils
+import data_utils as utils
 
 
-def make_sim():
+def get_common_simulation_pars():
+    # HIGH-LEVEL SIM PARAMETERS
+    pars = dict(
+        start    =2017.0,         # Start year
+        n_years  =6.0,            # Duration of the simulation in years
+        dt       =1.0/365.0,      # Timestep of 1 day, expressed in years
+        n_agents =10_000,         # Number of agents in the population
+        verbose  =0,              # Print details of the run
+    )
+    return pars
+
+
+def baseline_model():
     """
     Specify the complete model, and create a simulation instance of typhoid
     with a simple vaccination intervention.
@@ -19,22 +33,19 @@ def make_sim():
         None
 
     Returns:
-    sim (starsim.Sim): a starsim simulation object, configured and ready to run.
+    sim (starsim.Sim): a starsim simulation object, configured with the model we want to run.
     """
 
-    # HIGH-LEVEL SIM PARAMETERS
-    pars = dict(
-        start    =2017.0,         # Start year
-        n_years  =6.0,            # Duration of the simulation in years
-        dt       =1.0/365.0,      # Timestep of 1 day, expressed in years
-        verbose  =0,              # Pint details of the run
-    )
+    # The comon parameters to all models/simulations
+    pars = get_common_simulation_pars()
 
     # Loads age distribution data from the json file used in EMOD simulations
-    age_data = utils.get_age_distribution_pakistan()   # NOTE: Any adjustments to the age distribution (ie, to scale it to the demographics of the specific province) can be done here
+    # NOTE: Any adjustments to the age distribution needs to be done here ie,
+    #  to scale age_data to adjust to the demographics of a specific province
+    age_data = utils.get_age_distribution_pakistan()
 
     # POPULATION
-    ppl = ss.People(10_000, age_data=age_data)
+    ppl = ss.People(pars["n_agents"], age_data=age_data)
 
     # DEMOGRAPHICS
     # Load age-specific mortality rate, expressed in age-group proportions, in units of 1/year
@@ -55,9 +66,8 @@ def make_sim():
     sus_age_exposure_slope = 6.94   # ? not sure about the units
 
     # Function that defines the probability of becoming susceptible
-    p_unexp2sus_parc_fun = utils.partial_unexp2susc(
-        sus_saturation_age=sus_saturation_age,
-        sus_age_exposure_slope=sus_age_exposure_slope)
+    p_unexp2sus_parc_fun = partial_unexp2susc(sus_saturation_age=sus_saturation_age,
+                                              sus_age_exposure_slope=sus_age_exposure_slope)
 
     typhoids_pars = {'tai': 42_000,
                      'tpri': 0.5,
@@ -66,7 +76,7 @@ def make_sim():
                      'tppi': 0.98,
                      'p_cpg': 0.108,
                      'p_acute': ss.bernoulli(p=0.16),
-                     'init_prev': ss.bernoulli(p=0.05),                   # Initial prevalence: This is how we seed infections at the start of a simulation. In this case approx 5% of the total population of agents, will be infected at t=0
+                     'init_prev': ss.bernoulli(p=0.05),         # Initial prevalence: This is how we seed infections at the start of a simulation. In this case approx 5% of the total population of agents, will be infected at t=0
                      'p_unexp2sus': ss.bernoulli(p=p_unexp2sus_parc_fun)}
 
     typhoid = ty.Typhoid(pars=typhoids_pars)
@@ -78,21 +88,43 @@ def make_sim():
 
     # INTERVENTIONS: Vaccination campaigns
 
-    # Parameters of seasonal pattern
+    # Parameters of seasonal trapezoidal pattern
     seasonal_env_pars = {
         'period': 365.0,           # in days
-        'peak_start_doy': 0.0,  # day of the year
+        'peak_start_doy': 0.0,     # day of the year
         'ramp_up_dur': 175.26,     # duration in days
         'ramp_dw_dur': 100.0,      # duration in days
         'cutoff_dur': 20.0,        # duration in days
         'max_amp': 1.0}
-    # Seasonal pattern
-    trapezoidal_pattern = utils.partial_env_trapezoidal(seasonal_env_pars)
 
-    # Intervention that uses the seasonal pattern
-    exposure_modulation = ty.environmental_trapezoidal_modulation(efficacy=trapezoidal_pattern, start_year=2005.0)
+    # Seasonal environemental pattern
+    trapezoidal_pattern = partial_env_trapezoidal(seasonal_env_pars)
 
-    # Intervention with vaccination
+    # Intervention that applies the seasonal pattern to modulate the relative transmissibility/exposure environemnt -> people
+    exposure_modulation = ty.environmental_trapezoidal_modulation(efficacy=trapezoidal_pattern, start_year=pars["start"])
+
+    # Intervention base test. This mimics the imperfect/incomplete nature of empirical data through the lens of testing a fraction of the population.
+    test = ty.base_test(prob_t=0.3, prob_tp=1.0, eligibility=ty.eligibility_by_age)
+
+    # OBSERVATIONS AND REPORTING
+    # Create an analyzer that will provide the results we need to compare to target empirical data
+    age_bin_edges = [0, 2, 15, ty.max_age]
+    age_bin_labels = ['<2', '2-15', '15+']  # human readable labels
+
+    # Track cases by age and by sex -- this analyzer returns counts in number of agents, not people. Scaling can be performed offline.
+    anz = ty.histograms_by_age_sex_monitor(age_bins=age_bin_edges,
+                                           age_bin_labels=age_bin_labels)
+
+    model_definition = dict(pars=pars, people=ppl, diseases=[typhoid],
+                            demographics=vital_dynamics + [environment],
+                            interventions=[test, exposure_modulation],
+                            analyzers=[anz])
+
+    return model_definition
+
+
+def vaccination_model():
+    # Define the model of a vaccination intervention/campaign
     campaign_vax_2_5_yo = ty.vaccination_with_waning(
         start_year=2020.0,
         end_year=2022.0,
@@ -104,36 +136,73 @@ def make_sim():
                   'max_age': 4.0}
         )
 
-    # Intervention base test. This mimics the imperfect/incomplete nbature of empirical data through the lens of testing a fraction of the population.
-    test = ty.base_test(prob_t=0.3, prob_tp=1.0, eligibility=ty.eligibility_by_age)
+    model_definition = dict(interventions=[campaign_vax_2_5_yo])
+    return model_definition
 
-    # OBSERVATIONS AND REPORTING
-    # Create an analyzer that will provide the results we need to compare to target empirical data
-    age_bin_edges = [0, 2, 15, ty.max_age]
-    age_bin_labels = ['<2', '2-15', '15+']  # human readable labels
 
-    to_record = dict(ti_infected=dict(path=("diseases", "typhoid")),
-                     alive=dict(path=("people",)),
-                     ti_positive=dict(path=("interventions", "base_test")),
-                     ti_tested=dict(path=("interventions", "base_test")),
-                     tested=dict(path=("interventions", "base_test")),
-                     positive=dict(path=("interventions", "base_test")),
-                     )
-    # Track cases by age and by sex -- this analyzer returns counts in number of agents, not people. Scaling can be performed offline.
-    anz = ty.histograms_by_age_sex_monitor(age_bins=age_bin_edges, age_bin_labels=age_bin_labels, to_record=to_record)
+def make_sim(scenario="baseline"):
+    to_concat = ["diseases", "demographics", "interventions", "analyzers"]
+    match scenario:
+        case "baseline":
+            model = baseline_model()
+        case "with_vaccination_campaign":
+            baseline = baseline_model()
+            vaccination = vaccination_model()  # Add vaccination campaign
+            model = dict()
+            for key in set(baseline) | set(vaccination):
+                if key in to_concat:
+                    model[key] = baseline.get(key, []) + vaccination.get(key, [])
+                else:
+                    model[key] = baseline.get(key)
+        case _:
+            raise ValueError(f'Unrecognized simulation scenario: {scenario}')
 
     # PUT EVERYTHING TOGETHER IN A SIMULATION
-    sim = ss.Sim(pars=pars, people=ppl, diseases=typhoid, demographics=vital_dynamics + [environment],
-                 interventions=[test, exposure_modulation, campaign_vax_2_5_yo],
-                 analyzers=[anz])
-
+    sim = ss.Sim(**model)
     return sim
 
+
+def partial_unexp2susc(sus_saturation_age, sus_age_exposure_slope):
+    """
+    Create a partially evaluated function from unexp2susc_prob_function_gauld2018
+    with given sus_saturation_age and sus_age_exposure_slope arguments.
+
+    Args:
+        sus_saturation_age (float): The age at which exposure reaches saturation.
+        sus_age_exposure_slope (float): The slope of exposure with increasing age.
+
+    Returns:
+        callable: A partially evaluated function of unexp2susc_prob_function_gauld2018.
+                  In the simulation the ages of agents will be passed to this function,
+                  to determine whether the agent becomes suseptible.
+    """
+    return partial(ty.unexp2sus_youth_prob_function_gauld2018,
+                   sus_saturation_age=sus_saturation_age,
+                   sus_age_exposure_slope=sus_age_exposure_slope)
+
+
+def partial_env_trapezoidal(kwarg_pars):
+    """
+    Return a partially evaluated function. This means that the pattern is set
+    according to the parameters recieved in kwargs_pars. We can get exactly
+    the value of the environmental modulation at time t (expressed in years)
+    by calling
+
+    my_modulation = partial_env_trapezoidal(**kwargs_pars)
+    curent_modulation = my_modulation(t)
+
+    Args:
+        kwarg_pars of function typhoidsim.utils_math.asym_trapezoidal
+
+    Returns:
+         callable: A partially evaluated ty.asym_trapezoidal
+    """
+    return partial(ty.asym_trapezoidal, **kwarg_pars)
 
 
 def run_debug_single_sim(do_plot=True):
     """ Run one simulation"""
-    sim = make_sim()
+    sim = make_sim(scenario="baseline")
     sim.run()
     if do_plot:
         sim.plot()
@@ -164,3 +233,7 @@ def run_debug_multisim(do_plot=True):
         msim.plot()
         plt.show()
     return msim
+
+
+if __name__ == "__main__":
+    run_debug_single_sim()
