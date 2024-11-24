6f0e39e9d662d409d68518f8e36926ba1ef64ae5
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-11-24 15:06:42 +1000

    Test analyzer with setup similar to calibraiton

--- tests/dev_tests/test_histogram_analyzer.py
+++ tests/dev_tests/test_histogram_analyzer.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # Define high-level simulation parameters
 pars = dict(
-    start=2000,    # Starting year
+    start=2010,    # Starting year
     n_years=1.0,   # Duration of the simulation in years
     dt=1.0/365.0,  # Timestep of 1 day, expressed in years
     verbose=1,     # Do not print details of the run
@@ -26,15 +26,43 @@ age_bin_labels = ['<2', '2-4', '5-9', '10-14', '15-19', '20-39', '40-59', '60+']
 #                  ti_tested=dict(path=("interventions", "base_test")),
 #                  )
 
-anz_1 = ty.histograms_by_age_sex_monitor(age_bins=age_bin_edges,
-                                         age_bin_labels=age_bin_labels,
-                                         aggregate_sex=True,
-                                         name="hist_by_counts")
+# anz_1 = ty.histograms_by_age_sex_monitor(age_bins=age_bin_edges,
+#                                          age_bin_labels=age_bin_labels,
+#                                          aggregate_sex=True,
+#                                          name="hist_by_counts")
 
+age_bin_edges = [0, 2, 15, ty.max_age]
+age_bin_labels = ['<2', '2-15', '15+']  # human readable labels
 
-sim = ss.Sim(pars=pars, people=ppl, diseases=typhoid, interventions=tst, analyzers=[anz_1])
+# Track cases by age and by sex -- this analyzer returns counts in number of agents, not people. Scaling can be performed offline.
+record_cases = dict(ti_acute=dict(path=("diseases", "typhoid"), label="cases"))
+record_population = dict(alive=dict(path=("people",)))
+
+monitor_cases = ty.histograms_by_age_sex_monitor(age_bins=age_bin_edges,
+                                                 age_bin_labels=age_bin_labels,
+                                                 to_record=record_cases,
+                                                 resampling_period=30.44/365,
+                                                 # Record data on a montly basis, so we can aggregate later
+                                                 aggregate_sex=True,
+                                                 aggregate_time="sum",
+                                                 # Sum over the resampling period
+                                                 record_from=2010.0,
+                                                 name="monitor_1")
+
+monitor_population = ty.histograms_by_age_sex_monitor(age_bins=age_bin_edges,
+                                                      age_bin_labels=age_bin_labels,
+                                                      to_record=record_population,
+                                                      resampling_period=30.44/365.0,
+                                                      # Record data on a montly basis, so we can aggregate later
+                                                      aggregate_sex=True,
+                                                      aggregate_time="mean",
+                                                      # Average the number of people over the resampling period
+                                                      record_from=2010.0,
+                                                      name="monitor_2")
+
+sim = ss.Sim(pars=pars, people=ppl, diseases=typhoid, interventions=tst, analyzers=[monitor_cases, monitor_population])
 sim.run()
 timevec = sim.get_analyzers()[0].yearvec
-ty.plot_sim(sim, key="hist_by_", yearvec=timevec)
+ty.plot_sim(sim, key="monitor_", yearvec=timevec)
 sim.plot(key="typhoid_new")
 plt.show()
