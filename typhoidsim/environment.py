d0a873eca01a971f18c9e19db8f0065b44434c71
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-12-05 16:00:55 +1000

    Remove temperature-dependent growth of CFUs

--- typhoidsim/environment.py
+++ typhoidsim/environment.py
@@ -29,10 +29,6 @@ class EnvironmentalPool(ss.Demographics):
 
             ),
             teer_lam=2.0,     # HACKY parameter, otherwise CAlibration class does not ru where the path to the parameter is longer than 3
-            # Temperature-parameters, not used at the moment
-            bs_temp=6.0,   # Baseline temperature at which bacteria would stop growing, in degree Celsius
-            av_temp=14.0,  # typ.Pattern("av_temp", pars={'av_temp': 14.0}, pattern_name="Environmental Temperature"),
-            b=0.0297,      # fraction of change (increase or decrease) in [growth rate/degree Celsius]
         )
         self.update_pars(pars, **kwargs)
 
@@ -44,7 +40,6 @@ class EnvironmentalPool(ss.Demographics):
     def init_results(self):
         npts = self.sim.npts
         self.results += [
-            ss.Result(self.name, 'temperature', npts, dtype=int, scale=True, label='Environmental temperature'),
             ss.Result(self.name, 'cfu_conc', npts, dtype=float, scale=True, label='Current CFU concentration'),
             ss.Result(self.name, 'cfu_num', npts, dtype=int, scale=True, label='Current number of CFUs'),
             ss.Result(self.name, 'rel_trans', npts, dtype=float, scale=True, label='Relative exposure to long-cycle CCTV'),
@@ -56,9 +51,6 @@ class EnvironmentalPool(ss.Demographics):
         super().init_pre(sim)
         self.init_svs()
         self.init_env_pool(sim)  # Initialise the environmental pool of contagion at t-1
-        if sc.isnumber(self.pars.av_temp):
-            self.pars.av_temp = typ.Pattern("av_temp", pars={'av_temp': self.pars.av_temp})
-        ##
         self.pars.transmission.env2ppl_exposure_rate.lam = self.pars.teer_lam
         return
 
@@ -68,7 +60,6 @@ class EnvironmentalPool(ss.Demographics):
         """
         npts = self.sim.npts
         self.sv += [typ.StateVariable(self.name, "cfu_conc",    npts, dtype=float),]
-        self.sv += [typ.StateVariable(self.name, "temperature", npts, dtype=float),]
         return
 
     def init_env_pool(self, sim):
@@ -88,17 +79,14 @@ class EnvironmentalPool(ss.Demographics):
         sim = self.sim
         ti = self.sim.ti
         p = self.pars
-
         # For external changes that may promote bacterial growth
-        growth_rate = self.get_growth_rate()
-        change_rate = (p.decay_rate)
+        change_rate = p.decay_rate
         effective_rate = (change_rate / tyd.day2year)  # transform to yearly rate
-        self.sv.cfu_conc[ti-1] = self.sv.cfu_conc[ti-2] * np.exp(-effective_rate*self.sim.dt)  # + shedded into environment + decay
+        self.sv.cfu_conc[ti-1] = self.sv.cfu_conc[ti-2] * np.exp(-effective_rate*sim.dt)  # + shedded into environment + decay
         return
 
     def update_results(self):
         self.results['cfu_conc'][self.sim.ti] = self.sv.cfu_conc[self.sim.ti-1]
         self.results['cfu_num'][self.sim.ti]  = self.sv.cfu_conc[self.sim.ti-1] * self.pars.volume
-        self.results['temperature'][self.sim.ti] = self.sv.temperature[self.sim.ti-1]
         self.results['rel_trans'][self.sim.ti] = self.pars.transmission.rel_trans
         return
