406093ca52abf524f5f821fde038ead8511f5252
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-12-12 20:01:54 +1000

    Implement calib components and data extraction for incidence

--- examples/calibration/calibration_components.py
+++ examples/calibration/calibration_components.py
@@ -44,7 +44,7 @@ def parse_update_sim_pars(sim, calib_pars, **kwargs):
             case "teer":
                 env.pars.transmission.env2ppl_exposure_rate.lam = v
             case "rel_trans":
-                env.pars.transmission.rel_trans = v / sim.pars["n_agents"]
+                env.pars.transmission.rel_trans = v
             case "init_prev":
                 typh.pars.init_prev = ss.bernoulli(v)
             case _:
@@ -142,12 +142,14 @@ def make_calib_components_by_age_incidence(reference_data):
     - Population > Population_surveillance_corrected
 
     Sindh full time period: January 2019 - December 2023 (excluding lockdown)
+    # TODO: ability to pass kwargs with start and end year
     """
     components = []
     num_age_bins = reference_data.age_bin_label.nunique()
     for this_age_bin in sorted(reference_data.age_bin_label.unique()):
         expected_data = extract_reference_data_incidence(reference_data,
-                                                         selected_age_bin=this_age_bin)
+                                                         selected_age_bin=this_age_bin,
+                                                         start_year=2017.0, end_year=2024.0)
         extract_data_from_sim_fn = partial(extract_simulated_data_incidence,
                                            selected_age_bin=this_age_bin,
                                            start_year=2017.0, end_year=2024.0)
@@ -177,18 +179,21 @@ def extract_simulated_data_prevax(sim, selected_age_bin=None, start_year=2017.0,
          selected_age_bin (str): a human-readable labelf or the age bin of interest of the form 'age_lb-age_ub'.
              This age bin indicates the data are grouped by age in the interval [age_lb, age_ub). The
              age bin label comes from reference data.
-         start_year (float): this function aggregates data over time. This indicates the start year of the interval over which data are aggregated.
-         end_year   (float): This is the end year of the semi-opne interval over which dat are aggregated.
+         start_year (float): The initial year of the interval for data aggregation.
+              Data collection and aggregation commences from this year.
+         end_year   (float): The end year of the semi-open interval for data aggregation.
+             Data are aggregated up until, but not including, this year.
 
          Data are aggregated over the interval [start_year, end_year)
 
     Returns:
         simulated_data (pandas.Dataframe): with columns:
-         - 'n' number of observations; in this case is the total number of cases: sum over age bins and time interval of interest (start_year <= time < end year)
+         - 'n' number of observations; in this case is the total number of cases:
+             sum over age bins and time interval of interest (start_year <= time < end year)
             - 'n' is not always used during calibration, this depends on the likelihood function used
          - 'x' value of the metric of interest (ie, can be counts like new cases, or a proportion)
          - 'age_bin' a string with the human readable label of this age bin
-         - index is time; index name is 't'
+         - index is time; expected index name is 't', and so far it is expected to represent time
     """
 
     sim_results = sim.results.flatten()
@@ -222,6 +227,21 @@ def extract_reference_data_prevax(reference_data, selected_age_bin=None):
     plus the steps done in calculate_likelihood() to get exactly the data/quantity
     that is actually used to calculate likelihood/distance/cost during
     the calibration process.
+
+
+    Args:
+         reference_data (pandas DataFrame): a dataframe with the incidence data from
+             sheet "CasesByAge_prevax" and "Population". Includes uncorrected and
+             corrected values for cases_sum.The data have been aggregated in one single
+            time bin of 3 years[2017, 2020)
+        selected_age_bin (str): a human-readable labelf or the age bin of
+             interest of the form 'age_lb-age_ub'. This age bin indicates the
+             data are grouped by age in the interval [age_lb, age_ub). The
+             age bin label comes from reference data.
+
+    Returns:
+        expected_data (pandas DataFrame): a dataframe in the format ready to be
+        used by CalibComponents and Calibration classes can understand.
     """
 
     age_bin_mask = (reference_data["age_bin_label"] == selected_age_bin)
@@ -240,34 +260,110 @@ def extract_reference_data_prevax(reference_data, selected_age_bin=None):
     return expected_data
 
 
-def extract_simulated_data_incidene(reference_data, selected_age_bin=None, start_year=2017.0, end_year=2024.0):
-    pass
+def extract_simulated_data_incidence(sim, selected_age_bin=None, start_year=2017.0, end_year=2024.0):
+    """
+    Args:
+         sim (starsim Sim): a Sim object already run. Expects certain monitors to exist, as defined in models.py
+         selected_age_bin (str): a human-readable labelf or the age bin of interest of the form 'age_lb-age_ub'.
+             This age bin indicates the data are grouped by age in the interval [age_lb, age_ub). The
+             age bin label comes from reference data.
+
+        start_year (float): Specifies the initial year. Data selection will
+              commence from this year.
+         end_year   (float): Specifies the termination year. Data selection
+              will occur up to, but not including, this year.
+
+    Returns:
+        simulated_data (pandas DataFrame): a dataframe in the format ready to be
+            used by CalibComponents and Calibration classes can understand,
+            and its columns match those found in expected_data.
+    """
+    sim_results = sim.results.flatten()
+    cases_key = "monitor_1_b_new_acute"       # New cases (acute), summed over the period of the monitor/report which we have set to be 1 month, so we can exclude lockdown months
+    people_key = "monitor_2_b_n_alive"        # People
+
+
+    lbl_to_idx = sim.get_analyzers()[0].age_bin_lbl_to_idx  # Mapping between age bin string labels and index in the monitor results which are 2D arrays
+
+    yearvec = sim_results["monitor_1_yearvec"][:]  # The time vector of the monitored simulated data, expressed in "float" calendar years, ie 2000.0, 2000.1 ...
+
+    # Apply lockdown mask
+    not_lockdown = utils.lockdown_mask(yearvec)
+    # Select the correct interval of time
+    time_mask = ((yearvec >= start_year) & (yearvec < end_year) & not_lockdown)
+
+    # Age bin index
+    this_idx = lbl_to_idx[selected_age_bin]
+
+    # Build the dataframe the calibration component needs
+    years = np.arange(start_year, end_year)
+    year_masks = time_mask & (yearvec[None, :] == years[:, None])
+    x = np.sum(year_masks * sim_results[cases_key][:, this_idx], axis=1)
+    n = sim_results[people_key][np.argmax(year_masks, axis=1), :][:, 0]
+    year_index = years + 0.5
+    year_bin_labels = np.array([f"{int(year)}-{int(year + 1)}" for year in years])
+
+    simulated_data = pd.DataFrame(data={"n": n,
+                                        "x": x,
+                                        "age_bin": selected_age_bin,
+                                        "year_bin": year_bin_labels},
+                                  index=pd.Index(year_index, name="t"))
+    return simulated_data
 
 
 def extract_reference_data_incidence(reference_data, selected_age_bin=None, start_year=2017.0, end_year=2024.0):
     """
-    This function is similar to get_age_ref_data in AgeDistAnalyzer_Count.py,
+    This function is similar to get_age_ref_data in IncidenceRateAnalyzer.py,
     plus the steps done in calculate_likelihood() to get exactly the data/quantity
     that is actually used to calculate likelihood/distance/cost during
     the calibration process.
+
+    Args:
+         reference_data (pandas DataFrame): a dataframe with the incidence data from
+             sheet "CasesByAgeAll" and "Population". Includes uncorrected and corrected values
+             for cases and n_people.The data have been aggregated in 1y bins.
+
+         selected_age_bin (str): a human-readable labelf or the age bin of
+             interest of the form 'age_lb-age_ub'. This age bin indicates the
+             data are grouped by age in the interval [age_lb, age_ub). The
+             age bin label comes from reference data.
+
+         start_year (float): Specifies the initial year. Data selection will
+              commence from this year.
+         end_year   (float): Specifies the termination year. Data selection
+              will occur up to, but not including, this year.
+
+         The data selected will cover the interval [start_year, end_year).
+
+    Returns:
+        expected_data (pandas DataFrame): a dataframe in the format ready to be
+            used by CalibComponents and Calibration classes can understand.
+
+    expected_data should look like this (selected_age_bin="2-5")
+                      n       x age_bin   year_bin
+        0  1.258310e+06   526.0     2-5  2017-2018
+        1  1.292788e+06   982.0     2-5  2018-2019
+        2  1.328210e+06  1912.0     2-5  2019-2020
+        3  7.960184e+05   624.0     2-5  2020-2021
+        4  1.401993e+06  1970.0     2-5  2021-2022
+        5  1.440408e+06  2115.0     2-5  2022-2023
+        6  1.479875e+06  2780.0     2-5  2023-2024
     """
 
     age_bin_mask = (reference_data["age_bin_label"] == selected_age_bin)
     age_bin_ref_data = reference_data.loc[age_bin_mask, :]
 
-    year_bin_mask = ((age_bin_ref_data.year_start >= start_year) &
-                     (age_bin_ref_data.year_start.year_end < end_year))
+    year_bin_mask = ((age_bin_ref_data.year_start >= start_year) & (age_bin_ref_data.year_end <= end_year))  # NOTE: we have the condition <= end_year because we still need to select the last bin [end_year-1, end_year)
 
-    x = age_bin_ref_data.loc[year_bin_mask, ["cases_corrected"]].astype(float).to_numpy()[0][0]
-    n = age_bin_ref_data.loc[year_bin_mask, ["n_people_corrected"]].astype(float)
-    year_bin = age_bin_ref_data.loc[year_bin_mask, ["year_bin_label"]]
+    x = age_bin_ref_data.loc[year_bin_mask, ["cases_corrected"]].astype(float).to_numpy().flatten()
+    n = age_bin_ref_data.loc[year_bin_mask, ["n_people_corrected"]].astype(float).to_numpy().flatten()
+    year_bin = age_bin_ref_data.loc[year_bin_mask, ["year_bin_label"]].reset_index(drop=True)
 
     start = age_bin_ref_data.year_start
     end   = age_bin_ref_data.year_end
-    year_index = np.array([(start + end) / 2.0])   # Use the centre of the year bin as the time index
-    expected_data = pd.DataFrame(data={"n": n,
-                                       "x": x,
-                                       "age_bin": selected_age_bin,
-                                       "year_bin": year_bin},
+    year_index = np.array([(start + end) / 2.0]).flatten()   # Use the centre of the year bin as the time index
+    # NOTE: if using the beta binomial for NLL, pass x:cases and n:n_people, if using euclidean 'NLL' then probably need to pass x: x/n (incidence rate)
+    expected_data = pd.DataFrame(data={"n": n, "x": x, "age_bin":selected_age_bin,
+                                       "year_bin": year_bin.values.flatten()},
                                  index=pd.Index(year_index, name="t"))
     return expected_data
