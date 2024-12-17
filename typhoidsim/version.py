f9fde520b816848c108c9e9ae7b58d8f5411e1d7
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-12-17 18:16:43 +1000

    Bump patch version
    Kyra's fix in beta_binomial

--- typhoidsim/version.py
+++ typhoidsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.11.1'
+__version__ = '0.11.2'
 __versiondate__ = '2024-12-16'
 __license__ = f'Typhoidsim {__version__} ({__versiondate__}) — © 2024 by IDM'
