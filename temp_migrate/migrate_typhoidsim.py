17cae07c45cc1f8fb1968858c34c4e61809b741b
Cliff Kerr <questionably@gmail.com>
2024-12-16 23:01:19 -0500

    version of script to work with new version of code

---
+++ temp_migrate/migrate_typhoidsim.py
@@ -0,0 +1,32 @@
+"""
+Script to use Starsim-AI to migrate Typhoidsim
+"""
+
+import starsim as ss
+import starsim_ai as ssai
+
+# The files to migrate
+files = [
+    'calibration.py',
+    'demographics.py',
+    'environment.py',
+    'interventions.py',
+    'monitors.py',
+    'networks.py',
+    'patterns.py',
+    'typhoid.py',
+    'utils.py',
+]
+
+M = ssai.Migrate(
+    source_dir = '../typhoidsim', # folder with the code to migrate
+    dest_dir = './migrated', # folder to output migrated code into
+    files = files, # the specific files to migrate
+    library = ss, # can also be the path to the starsim folder, which must be the cloned repo (not from pypi)
+    v_from = 'v1.0.3', # can be any valid git tag or hash
+    v_to = 'v2.2.0', # ditto
+    model = 'gpt-4o', # see ssai.Models for list of allowed models
+    parallel = False,
+    die = True,
+)
+M.run()
