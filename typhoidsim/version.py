c7a33af11feaeba9939c489d59ba54d44d901d42
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-12-19 08:28:07 +1000

    Add excel file with age-mixing data for CommunitNet

--- typhoidsim/version.py
+++ typhoidsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.11.3'
-__versiondate__ = '2024-12-16'
+__version__ = '0.11.4'
+__versiondate__ = '2024-12-19'
 __license__ = f'Typhoidsim {__version__} ({__versiondate__}) — © 2024 by IDM'
