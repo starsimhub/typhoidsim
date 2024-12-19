9cd96e5976d59a55f0529ddd9d0f3f2e5697b2e1
Cliff Kerr <questionably@gmail.com>
2024-12-19 16:48:12 -0500

    runs with wrong but not completely wrong results

--- tests/simple.py
+++ tests/simple.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 # Define the parameters
 pars = dict(
     n_agents = 10e3,
-    start = '2000-01-01',
-    stop = '2002-01-01',
-    unit = 'day',
-    dt = 1.0,
+    start = 2000,
+    stop = 2002,
+    unit = 'year',
+    dt = 1/365,
 )
 
 typh = ty.Typhoid()
