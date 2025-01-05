a2b296dca6647ae3172de60d5b5762a578ccedf7
Paula Sanz-Leon <pmsl.academic@gmail.com>
2025-01-05 16:56:04 +1000

    [TESTS]: update vital dynamics tes: uses starsim births

--- tests/dev_tests/test_vital_dynamics.py
+++ tests/dev_tests/test_vital_dynamics.py
@@ -10,9 +10,8 @@ import typhoidsim as ty
 # Define the parameters
 pars = sc.objdict(
     start=2000,       # Starting year
-    n_years=1.0,      # Number of days to simulate
+    dur=1.0,      # Number of days to simulate
     dt=1.0/365.0,     # Timestep of 1 day, expressed in years
-    verbose=1,        # Print details of the run
     rand_seed=2,      # Set a non-default seed
 )
 
@@ -22,7 +21,7 @@ ppl = ss.People(10_000)
 
 
 demographics = [
-    ty.Births(birth_rate=20),
+    ss.Births(birth_rate=20),
     ss.Deaths(death_rate=15)
 ]
 
@@ -34,8 +33,7 @@ sim = ss.Sim(
     diseases=typhoid,
     demographics=demographics
     )
-sc.tic()
+
 sim.run()
-sc.toc()
 sim.plot()
 plt.show()
