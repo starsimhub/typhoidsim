5ab3dca0272fe0f69cf8ebcc06a42e22780b4360
Paula Sanz-Leon <pmsl.academic@gmail.com>
2025-01-10 09:12:40 +1000

    Make sure beahvioural change is applied once

--- typhoidsim/interventions.py
+++ typhoidsim/interventions.py
@@ -557,7 +557,7 @@ class behavioral_change(WASH):
         return
 
     def step(self):
-        if self.sim.t.now('year') >= self.start and len(self.time):
+        if self.sim.t.now('year') == self.start:
             efficacy = self.efficacy_pattern(self.time[0])
             self.time = self.time[1:]
             val = self._get_target_baseline()
