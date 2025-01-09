569a5f050b44fc39f0eb1912645256dfd9ffc90f
Paula Sanz-Leon <pmsl.academic@gmail.com>
2025-01-10 09:47:18 +1000

    Bump minor version

--- typhoidsim/version.py
+++ typhoidsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.22.0'
+__version__ = '0.23.0'
 __versiondate__ = '2025-01-10'
 __license__ = f'Typhoidsim {__version__} ({__versiondate__}) — © 2024-2025 by IDM'
