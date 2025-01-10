fc99a5ea339d43085944068a4b08ce59a652fea6
Paula Sanz-Leon <pmsl.academic@gmail.com>
2025-01-10 12:55:39 +1000

    Tidy up monitor dev test

--- tests/dev_tests/test_histogram_analyzer.py
+++ tests/dev_tests/test_histogram_analyzer.py
@@ -11,40 +11,29 @@ pars = dict(
 )
 
 ppl = ss.People(10_000)
-init_p = 0.5 # Large prevalence to see results
+init_p = 0.05 # Large prevalence to see results
 typhoid = ty.Typhoid(pars={'init_prev': ss.bernoulli(p=init_p)})
 # create and apply the test intervention
 
-blood_test   = ty.typhoid_test(pars=dict(sensitivity=ss.bernoulli(p=1.0)))
-screen_all= ty.routine_acute_screening(product=blood_test, prob=0.3, eligibility=ty.eligibility_by_age)  # Screen 30% of all individuals
+blood_test   = ty.typhoid_test(pars=dict(sensitivity=ss.bernoulli(p=0.65)))
+screen_all= ty.routine_acute_screening(product=blood_test, prob=0.3, annual_prob=False,
+                                       eligibility=ty.eligibility_by_age)  # Screen 30% of all eligible population at each time step
 
 age_bin_edges = [0, 2, 5, 10, 15, 20, 40, 60, ty.max_age]
 age_bin_labels = ['<2', '2-4', '5-9', '10-14', '15-19', '20-39', '40-59', '60+']
-
-# Track cases by age and by sex -- this analyzer returns counts in number of agents, not people. Scaling can be performed offline.
-
-# to_record = dict(ti_infected=dict(path=("diseases", "typhoid")),
-#                  alive=dict(path=("people",)),
-#                  ti_positive=dict(path=("interventions", "base_test")),
-#                  ti_tested=dict(path=("interventions", "base_test")),
-#                  )
-
-# anz_1 = ty.histograms_by_age_sex_monitor(age_bins=age_bin_edges,
-#                                          age_bin_labels=age_bin_labels,
-#                                          aggregate_sex=True,
-#                                          name="hist_by_counts")
 
 age_bin_edges = [0, 2, 15, ty.max_age]
 age_bin_labels = ['<2', '2-15', '15+']  # human readable labels
 
 # Track cases by age and by sex -- this analyzer returns counts in number of agents, not people. Scaling can be performed offline.
-record_cases = dict(ti_acute=dict(path=("diseases", "typhoid"), label="cases"))
+record_cases = dict(ti_infected=dict(path=("diseases", "typhoid"), label="infected"))
+
 record_population = dict(alive=dict(path=("people",)))
 
 monitor_cases = ty.histograms_by_age_sex_monitor(age_bins=age_bin_edges,
                                                  age_bin_labels=age_bin_labels,
                                                  to_record=record_cases,
-                                                 resampling_period=30.44/365,
+                                                 resampling_period=7.0/365,
                                                  # Record data on a montly basis, so we can aggregate later
                                                  aggregate_sex=True,
                                                  aggregate_time="sum",
@@ -55,7 +44,7 @@ monitor_cases = ty.histograms_by_age_sex_monitor(age_bins=age_bin_edges,
 monitor_population = ty.histograms_by_age_sex_monitor(age_bins=age_bin_edges,
                                                       age_bin_labels=age_bin_labels,
                                                       to_record=record_population,
-                                                      resampling_period=30.44/365.0,
+                                                      resampling_period=7.0/365.0,
                                                       # Record data on a montly basis, so we can aggregate later
                                                       aggregate_sex=True,
                                                       aggregate_time="mean",
@@ -65,6 +54,5 @@ monitor_population = ty.histograms_by_age_sex_monitor(age_bins=age_bin_edges,
 
 sim = ss.Sim(pars=pars, people=ppl, diseases=typhoid, interventions=screen_all, analyzers=[monitor_cases, monitor_population])
 sim.run()
-timevec = sim.analyzers[0].timevec_
 sim.plot()
 plt.show()
