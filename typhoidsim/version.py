eb7f3441815c7a5fab046b82429814019f06eb9e
Paula Sanz-Leon <pmsl.academic@gmail.com>
2025-01-23 20:38:23 +1000

    Increment minor version

--- typhoidsim/version.py
+++ typhoidsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.25.2'
+__version__ = '0.26.0'
 __versiondate__ = '2025-01-23'
 __license__ = f'Typhoidsim {__version__} ({__versiondate__}) — © 2024-2025 by IDM'
