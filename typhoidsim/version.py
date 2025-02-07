2bbfb3ffd4ca22e33e3773d5ad9fafe1baedbdb7
Paula Sanz-Leon <pmsl.academic@gmail.com>
2025-02-07 21:01:28 +1000

    Increment patch version

--- typhoidsim/version.py
+++ typhoidsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.26.8'
-__versiondate__ = '2025-01-30'
+__version__ = '0.26.9'
+__versiondate__ = '2025-02-07'
 __license__ = f'Typhoidsim {__version__} ({__versiondate__}) — © 2024-2025 by IDM'
