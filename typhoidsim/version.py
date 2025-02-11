107c7bd3c9d59a2a1d8fa3798c4166f705ff81b6
Kyra Grantz <kyragrantz@gmail.com>
2025-02-11 10:42:38 -0800

    bump version #202

--- typhoidsim/version.py
+++ typhoidsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.27.0'
-__versiondate__ = '2025-02-07'
+__version__ = '0.28.0'
+__versiondate__ = '2025-02-11'
 __license__ = f'Typhoidsim {__version__} ({__versiondate__}) — © 2024-2025 by IDM'
