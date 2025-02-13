79cc9f05ebefeac5abc6ec1e3c437575672888d9
Paula Sanz-Leon <pmsl.academic@gmail.com>
2025-02-13 15:51:19 +1000

    Use pyproject

--- typhoidsim/version.py
+++ typhoidsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.28.0'
-__versiondate__ = '2025-02-11'
+__version__ = '0.28.1'
+__versiondate__ = '2025-02-13'
 __license__ = f'Typhoidsim {__version__} ({__versiondate__}) — © 2024-2025 by IDM'
