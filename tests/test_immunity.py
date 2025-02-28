9cdb2eb25d15ca9aa328b0e5e31cc628ac3b17f5
Paula Sanz-Leon <pmsl.academic@gmail.com>
2025-02-28 21:11:31 +1000

    Add basic test for new immunity functions

---
+++ tests/test_immunity.py
@@ -0,0 +1,24 @@
+"""
+Test immunity functions
+"""
+import typhoidsim as ty
+
+
+def test_imm_decay_by_age_default():
+    # Test for valid inputs
+    age_bins = [0.75, 2.0, 5.0, 15.0, 125.0]
+    vals = [505.27, 505.27, 505.27, 505.27]
+    default_function = ty.imm_decay_by_age_default(age_bins=age_bins, vals=vals)
+    assert callable(default_function)
+
+    # Test if the returned function is giving expected results
+    # The value expected is corresponding to the bin of the age value
+    assert default_function(1) == vals[0] / 365  # age falls in first bin
+    assert default_function(3) == vals[1] / 365  # age falls in second bin
+
+    # Test for None inputs
+    default_function_none = ty.imm_decay_by_age_default()
+    assert callable(default_function_none)
+    assert default_function_none(
+        1) == 505.27 / 365  # Check for expected default value
+
