8589cbddbac7aeb43055df918816631e8a50c66c
Paula Sanz-Leon <pmsl.academic@gmail.com>
2025-02-28 21:05:52 +1000

    Document

--- typhoidsim/immunity.py
+++ typhoidsim/immunity.py
@@ -15,26 +15,45 @@ __all__ = ['imm_decay_by_age_default', 'imm_constant_dur_by_age_default', 'imm_v
 
 
 def imm_decay_by_age_default(age_bins=None, vals=None):
+    """
+    Args:
+        age_bins (list | np.array): List of age values defining bins. Length n.
+        vals (list | np.array): List of values for each age bin. Length n-1
+
+    Returns:
+        callable: a function that takes as input the age of the agent (single float or array) and
+         returns the appropriate value.
+    """
     age_bins = sc.promotetoarray(age_bins) if age_bins is not None else np.array([0.75, 2.0, 5.0, 15.0, 125.0])  # Age at vaccination bins
     vals = sc.promotetoarray(vals) if vals is not None else np.array([505.27, 505.27, 505.27, 505.27])  # Decay time constant, in days, one value per age bin of interest Maximum protection at t=0 of receiving a vaccine
     return tyu.stratify_parameter_by_age(age_bins, vals / tyd.days_per_year)
 
 
 def imm_ve0_by_age_default(age_bins=None, vals=None):
+    """
+    Args:
+        age_bins (list | np.array): List of age values defining bins. Length n.
+        vals (list | np.array): List of values for each age bin. Length n-1
+
+    Returns:
+        callable: a function that takes as input the age of the agent (single float or array) and
+         returns the appropriate value.
+    """
     age_bins = sc.promotetoarray(age_bins) if age_bins is not None else np.array([0.75, 2.0, 5.0, 15.0, 125.0])  # Age at vaccination bins
     vals = sc.promotetoarray(vals) if vals is not None else np.array([1.0, 1.0, 1.0, 1.0])  # Maximum protection at t=0 of receiving a vaccine
     return tyu.stratify_parameter_by_age(age_bins, vals)
 
 
 def imm_constant_dur_by_age_default(age_bins=None, vals=None):
+    """
+    Args:
+        age_bins (list | np.array): List of age values defining bins. Length n.
+        vals (list | np.array): List of values for each age bin. Length n-1
+
+    Returns:
+        callable: a function that takes as input the age of the agent (single float or array) and
+         returns the appropriate value.
+    """
     age_bins = sc.promotetoarray(age_bins) if age_bins is not None else np.array([0.75, 2.0, 5.0, 15.0, 125.0])  # Age at vaccination bins
     vals = sc.promotetoarray(vals) if vals is not None else np.array([940.4, 240.9, 0.0, 0.0])  # Duration of constant immunity in days, one value per age bin of interest, assuming a box exponential model of immunity dynamics
     return tyu.stratify_parameter_by_age(age_bins, vals / tyd.days_per_year)
-
-
-def imm_constant_dur_fast(age_bins=None):
-    # Get parameters of the distribution by age
-    # Get the distribution parameter according to the age of the agent
-    # Draw values for
-
-    pass
