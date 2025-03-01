19bb45cad402c35bb0b01c505d212d5e23e9fb18
Paula Sanz-Leon <pmsl.academic@gmail.com>
2025-03-01 19:46:30 +1000

    Add gausshyper and gompertz distributions

--- tests/test_distributions.py
+++ tests/test_distributions.py
@@ -8,10 +8,9 @@ import matplotlib.pyplot as plt
 
 
 def test_truncnorm(n_samples=1000, a_trunc=0, b_trunc=2_000):
-    """ Test the Dist class """
-    sc.heading('Testing the basic Dist call')
+    sc.heading('Testing truncnorm')
     mu, sigma = 1_000, 1_000
-    dist = ty.truncnorm(a_trunc=a_trunc, b_trunc=b_trunc, loc=mu, scale=sigma, name='test_runcnorm', strict=False)
+    dist = ty.truncnorm(a_trunc=a_trunc, b_trunc=b_trunc, loc=mu, scale=sigma, name='test_truncnorm', strict=False)
     dist.init()
     rvs = dist(n_samples)
     print(rvs)
@@ -22,6 +21,29 @@ def test_truncnorm(n_samples=1000, a_trunc=0, b_trunc=2_000):
     return rvs
 
 
+def test_gausshyper(n_samples=1000):
+    sc.heading('Testing gausshyper')
+    a = 13.8
+    b = 3.12
+    c = 2.51
+    z = 5.18
+    loc = 1.0
+    dist = ty.gausshyper(a=a, b=b, c=c, z=z, loc=loc, strict=False)
+    dist.init()
+    rvs = dist(n_samples)
+    dist.plot_hist()
+
+
+def test_gompertz(n_samples=1000):
+    sc.heading('Testing gompertz')
+    dist = ty.gompertz(c=1.0, strict=False)
+    dist.init()
+    rvs = dist(n_samples)
+    dist.plot_hist()
+
+
 if __name__ == '__main__':
     test_truncnorm()
+    test_gausshyper()
+    test_gompertz()
     plt.show()
