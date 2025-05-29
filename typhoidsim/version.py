316e19c72fed94265bf96b033619e806c3a8285c
Kyra Grantz <kyragrantz@gmail.com>
2025-05-29 10:57:22 -0700

    update documentation

--- typhoidsim/version.py
+++ typhoidsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.30.6'
-__versiondate__ = '2025-03-08'
+__version__ = '0.31.0'
+__versiondate__ = '2025-05-29'
 __license__ = f'Typhoidsim {__version__} ({__versiondate__}) — © 2024-2025 by IDM'
