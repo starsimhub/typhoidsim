466cfec209f43cc21d6ce41a802b2f0e00c3699a
Cliff Kerr <questionably@gmail.com>
2026-06-07 23:58:08 -0400

    Built site for gh-pages

--- typhoidsim/version.py
+++
@@ -1,9 +0,0 @@
-"""
-Version and license information.
-"""
-
-__all__ = ['__version__', '__versiondate__', '__license__']
-
-__version__ = '1.0.0'
-__versiondate__ = '2026-06-07'
-__license__ = f'Typhoidsim {__version__} ({__versiondate__}) — © 2024-2026 by IDM'
